87663055ae47212bc5fa4998635cece1d7326384
Khaled Sharif <kldsrf@gmail.com>
2014-09-06 13:11:10 +0200

    Start of Secure Sync.

---
+++ secure-sync/Encryption.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Security;
+using System.Security.Cryptography;
+using System.Runtime.InteropServices;
+using System.Text.RegularExpressions;
+
+
+namespace SecureSync
+{
+    class Encryption
+    {
+        private byte[] RSAEncrypt(
+            byte[] DataToEncrypt, RSAParameters RSAKeyInfo, bool DoOAEPPadding)
+        {
+            try
+            {
+                RSACryptoServiceProvider RSAalg = new RSACryptoServiceProvider();
+                RSAalg.ImportParameters(RSAKeyInfo);
+                return RSAalg.Encrypt(DataToEncrypt, DoOAEPPadding);
+            }
+            catch (CryptographicException e)
+            {
+                Console.WriteLine(e.Message);
+                return null;
+            }
+        }
+
+        private byte[] RSADecrypt(
+            byte[] DataToDecrypt, RSAParameters RSAKeyInfo, bool DoOAEPPadding)
+        {
+            try
+            {
+                RSACryptoServiceProvider RSAalg = new RSACryptoServiceProvider();
+                RSAalg.ImportParameters(RSAKeyInfo);
+                return RSAalg.Decrypt(DataToDecrypt, DoOAEPPadding);
+            }
+            catch (CryptographicException e)
+            {
+                Console.WriteLine(e.ToString());
+                return null;
+            }
+        }
+
+        public byte[] EncryptFile(
+            RSAParameters rsa_public_key, byte[] file_to_be_encrypted)
+        {
+            byte[] encrypted_file;
+            try
+            {
+                encrypted_file = RSAEncrypt(file_to_be_encrypted, rsa_public_key, false);
+                return encrypted_file;
+            }
+            catch
+            {
+                Console.WriteLine("Can't open files and/or write to files.");
+                return null;
+            }
+        }
+
+        public byte[] DecryptFile(
+            RSAParameters rsa_private_key, byte[] file_to_be_decrypted)
+        {
+            byte[] decrypted_file;
+            try
+            {
+                decrypted_file = RSADecrypt(file_to_be_decrypted, rsa_private_key, false);
+                return decrypted_file;
+            }
+            catch
+            {
+                Console.WriteLine("Can't open files and/or write to files.");
+                return null;
+            }
+        }
+    }
+}
---
+++ secure-sync/Secure.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+
+namespace SecureSync
+{
+    class Secure
+    {
+        private AesCryptoServiceProvider m_des = new AesCryptoServiceProvider();
+
+        private byte[] m_key;
+        private byte[] m_iv;
+
+        private Random random_seed;
+
+        public Secure(byte[] key)
+        {
+            this.m_key = key;
+            this.random_seed = new Random(BitConverter.ToInt32(this.m_key, 0));
+            this.m_iv = new byte[16];
+            this.random_seed.NextBytes(this.m_iv);
+        }
+
+        public byte[] Encrypt(byte[] input)
+        {
+            return Transform(input, m_des.CreateEncryptor(m_key, m_iv));
+        }
+
+        public byte[] Decrypt(byte[] input)
+        {
+            return Transform(input, m_des.CreateDecryptor(m_key, m_iv));
+        }
+
+        private byte[] Transform(byte[] input, ICryptoTransform CryptoTransform)
+        {
+            MemoryStream memStream = new MemoryStream();
+            CryptoStream cryptStream = new CryptoStream(memStream, CryptoTransform, CryptoStreamMode.Write);
+            cryptStream.Write(input, 0, input.Length);
+            cryptStream.FlushFinalBlock();
+            memStream.Position = 0;
+            byte[] result = memStream.ToArray();
+            memStream.Close();
+            cryptStream.Close();
+            return result;
+        }
+    }
+}
